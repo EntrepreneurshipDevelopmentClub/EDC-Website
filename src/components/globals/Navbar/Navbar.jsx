4c2aeb5b04d88d59d567cb761a1cd03346277935
Yuuki-Nagisa <paul.dibyojit2005@gmail.com>
2026-02-07 10:14:45 +0530

    optimise Logo for performance

--- src/components/globals/Navbar/Navbar.jsx
+++ src/components/globals/Navbar/Navbar.jsx
@@ -3,47 +3,47 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="logo">
-                <Image
-                    src="/assets/brand-logo/logo2.png"
-                    alt="Logo"
-                    width={100}
-                    height={100}
-                    loading="lazy"
-                />
-            </div>
-            <input type="checkbox" id="check" />
-            <label htmlFor="check" className="checkbtn">
-                <i className="fa-solid fa-bars"></i>
-            </label>
+  return (
+    <nav>
+      <div className="logo">
+        <Image
+          src="/assets/brand-logo/logo2.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          loading="lazy"
+        />
+      </div>
+      <input type="checkbox" id="check" />
+      <label htmlFor="check" className="checkbtn">
+        <i className="fa-solid fa-bars"></i>
+      </label>
 
-            <ul className="list">
-                <li id="active">
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/about">About Us</Link>
-                </li>
-                <li>
-                    <Link href="/events">Events</Link>
-                </li>
-                <li>
-                    <Link href="/team">Team</Link>
-                </li>
-                <li>
-                    <Link href="/gallery">Gallery</Link>
-                </li>
-                <li>
-                    <Link href="#Contact">Reach Us</Link>
-                </li>
-                <li>
-                    <Link href="/udbhav">E-Summit 24</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+      <ul className="list">
+        <li id="active">
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About Us</Link>
+        </li>
+        <li>
+          <Link href="/events">Events</Link>
+        </li>
+        <li>
+          <Link href="/team">Team</Link>
+        </li>
+        <li>
+          <Link href="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link href="#Contact">Reach Us</Link>
+        </li>
+        <li>
+          <Link href="/udbhav">UDBHAV 26</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
